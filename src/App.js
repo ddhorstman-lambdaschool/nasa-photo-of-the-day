4c7421919c449791147bfffb9aae8b2c2968c4cd
David Horstman <davidzilla27@gmail.com>
2020-02-06 21:35:58 -0500

    Refactor DatePicker with Prev/Next Buttons

--- src/App.js
+++ src/App.js
@@ -6,22 +6,31 @@ import stringifyDate from "./components/StringifyDate";
 import "./App.css";
 
 function App() {
-  const currentDate = new Date();
-  let [date, setDate] = useState(stringifyDate(currentDate, "Y-m-d"));
-  const [apodData, setApodData] = useState({
+  const starterApodData = {
     date: "",
     explanation: "",
     media_type: "",
     title: "Loading Content",
     url: ""
-  });
+  };
+  let currentDate = new Date();
+  //if the date doesn't match the UTC date, use yesterday instead
+  if (currentDate.getHours() - currentDate.getTimezoneOffset() / 60 < 0) {
+    currentDate = new Date(Date.now() - 1000 * 60 * 60 * 24);
+  }
+  let [date, setDate] = useState(currentDate);
+  const [apodData, setApodData] = useState(starterApodData);
   useEffect(() => {
+    //setApodData(starterApodData);
     axios
       .get(
-        `https://api.nasa.gov/planetary/apod?api_key=knlstgdvAzayUHYdQaCAWQqPNlmdjLWeROosNjGd&date=${date}`
+        `https://api.nasa.gov/planetary/apod?api_key=knlstgdvAzayUHYdQaCAWQqPNlmdjLWeROosNjGd&date=${stringifyDate(
+          date,
+          "Y-m-d"
+        )}`
       )
       .then(r => setApodData(r.data))
-      .catch(e => console.log(e));
+      .catch(e => console.error(e));
   }, [date]);
   return (
     <div className="App">
--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,19 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
+import { Button } from "reactstrap";
 export default function DatePicker({ date, setDate }) {
-  const [currentDate, updateCurrentDate] = useState(date);
   return (
     <div>
-      <label htmlFor="date-picker">Select date: </label>
-      <input
-        id="date-picker"
-        type="date"
-        value={currentDate}
-        max={currentDate}
-        onChange={e => {
-          updateCurrentDate(e.target.value);
-          setDate(e.target.value);
-        }}
-      />
+      <Button
+        onClick={() => setDate(new Date(date.getTime() - 1000 * 60 * 60 * 24))}
+      >
+        Previous
+      </Button>{" "}
+      <Button
+        onClick={() => setDate(new Date(date.getTime() + 1000 * 60 * 60 * 24))}
+      >
+        Next
+      </Button>
     </div>
   );
 }
+/*
+<label htmlFor="date-picker">Select date: </label>
+  <!--<input
+    id="date-picker"
+    type="date"
+    value={currentDate}
+    max={currentDate}
+    onChange={e => {
+      updateCurrentDate(e.target.value);
+      setDate(e.target.value);
+    }}
+  />
+*/
